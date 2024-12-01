350f34229dbd0da5912696fe0ffc0af14f9cc02f
Muhoza John <muhozajohn250@gmail.com>
2024-12-01 16:04:38 +0200

    Adding login/contact form

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './login.scss'
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login-container">
+      <div className="login-form">
+        <h2>Welcome Back</h2>
+        <form>
+          <div className="form-group">
+            <input type="email" placeholder="Email" required />
+          </div>
+          <div className="form-group">
+            <input type="password" placeholder="Password" required />
+          </div>
+          <button type="submit" className="login-btn">
+            Login
+          </button>
+        </form>
+        
+        <div className="divider">
+          <span>or</span>
+        </div>
+
+        <button className="google-btn">
+          <FcGoogle />
+          Continue with Google
+        </button>
+      </div>
+    </div>
   )
 }
 
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./contact.scss";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="contact">
+      <h1 className="contact__title">Get in Touch </h1>
+      <div className="contact__wrapper">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+          exercitationem. Expedita, in! Laboriosam perferendis eaque debitis
+          facilis, dicta quidem rem.
+        </p>
+        <form className="contact__form">
+          <div className="contact__input-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+
+          <div className="contact__input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className="contact__input-group">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" required></textarea>
+          </div>
+
+          <button type="submit" className="contact__button">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
