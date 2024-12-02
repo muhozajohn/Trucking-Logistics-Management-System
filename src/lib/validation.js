942b3c303a2c9419a189d418ca0ebd49809b0a0f
Muhoza John <muhozajohn250@gmail.com>
2024-12-02 16:41:59 +0200

    Adding login

---
+++ src/lib/validation.js
@@ -0,0 +1,14 @@
+export const validateLoginForm = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = "Invalid email address";
+    }
+  
+    if (!values.password) {
+      errors.password = "Password is required";
+    }
+    return errors;
+  };
+  
