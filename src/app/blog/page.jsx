4efdf2e4b9ab95988f70a4cd5f39c221636c2e79
Muhoza John <muhozajohn250@gmail.com>
2024-12-02 09:10:28 +0200

    adding contact/blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./blog.scss";
+import Image from "next/image";
+import { blogPosts } from "@/components/json";
 
 const BlogPage = () => {
-  return <div>blog</div>;
+
+
+  return (
+    <div className="blog">
+      <h1 className="blog__title">Our Blog</h1>
+      <div className="blog__grid">
+        {blogPosts.map((post) => (
+          <div key={post.id} className="blog__card">
+            <div className="blog__image-container">
+              <Image src={post.image} alt={post.title} className="blog__image" />
+            </div>
+            <div className="blog__content">
+              <h2 className="blog__card-title">{post.title}</h2>
+              <p className="blog__description">{post.description}</p>
+              <button className="blog__read-more">Read More</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogPage;
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./contact.scss";
+import { IoHome } from "react-icons/io5";
+import { FaPhoneVolume } from "react-icons/fa";
+import { SiMinutemailer } from "react-icons/si";
 
 const ContactPage = () => {
   return (
     <div className="contact">
       <h1 className="contact__title">Get in Touch </h1>
       <div className="contact__wrapper">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
-          exercitationem. Expedita, in! Laboriosam perferendis eaque debitis
-          facilis, dicta quidem rem.
-        </p>
         <form className="contact__form">
+          <div className="contact__input-group">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" name="message" required></textarea>
+          </div>
           <div className="contact__input-group">
             <label htmlFor="name">Name</label>
             <input type="text" id="name" name="name" required />
@@ -22,15 +24,35 @@ const ContactPage = () => {
             <input type="email" id="email" name="email" required />
           </div>
 
-          <div className="contact__input-group">
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" required></textarea>
-          </div>
-
           <button type="submit" className="contact__button">
             Send Message
           </button>
         </form>
+        <div className="contact__content">
+          <div className="contact__content-item">
+            <IoHome className="contact__content-icon" />
+            <div>
+              <p>Rwanda</p>
+              <p>Kigali, kg st 654</p>
+            </div>
+          </div>
+          <div className="contact__content-item">
+            <FaPhoneVolume className="contact__content-icon" />
+
+            <div>
+              <p>+250 784 635 871</p>
+              <p>Mon to Fri 9am to 6pm</p>
+            </div>
+          </div>
+          <div className="contact__content-item">
+            <SiMinutemailer className="contact__content-icon" />
+
+            <div>
+              <p>ironji.sales@gmail.com </p>
+              <p>Send us your query anytime!</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
