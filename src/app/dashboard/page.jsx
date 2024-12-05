68c0e7efea1121189614605556f115e9208bc444
Muhoza John <muhozajohn250@gmail.com>
2024-12-05 13:45:59 +0200

    Adding Auth

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ProtectedRoute from "../../components/ProtectedRoute";
+import Trucks from "./trucks/page";
 
 const Dashboard = () => {
   return (
     <ProtectedRoute>
       <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, officia
-        porro, amet nobis eveniet delectus voluptatibus maxime ab quas ea
-        architecto dignissimos ut vero explicabo totam ad, laboriosam corporis
-        dolorem.
+       <Trucks />
       </div>
     </ProtectedRoute>
   );
